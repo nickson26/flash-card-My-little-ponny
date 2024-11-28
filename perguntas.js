a48bd3664d9e539f897b0584acc746a8a23c4f6e
nickson26 <nickson.olimpio.2022@icloud.com>
2024-11-28 19:24:54 -0300

    versão 0.1

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    "My Little Pony", 
+    "Qual é o nome da protagonista de *My Little Pony: A Amizade é Mágica*?",  
+    "Twilight Sparkle é a protagonista da série"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Qual pônei é conhecido por sua paixão por moda?",
+    "Rarity é a pônei que adora moda e é uma talentosa designer"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Quem é a princesa que governa Equestria?",
+    "Princesa Celestia governa Equestria e controla o sol"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Qual pônei ama fazer festas e é super animado?",
+    "Pinkie Pie é conhecida por seu amor por festas e sua energia contagiante"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Qual pônei representa o Elemento da Lealdade?",
+    "Rainbow Dash representa o Elemento da Lealdade"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Qual é o nome da cidade onde as pôneis vivem?",
+    "Ponyville é a cidade onde as pôneis vivem"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Quem é a irmã mais nova da Princesa Celestia?",
+    "Princesa Luna é a irmã mais nova da Princesa Celestia"
+);
+
+criaCartao(
+    "My Little Pony",
+    "Qual pônei ama cuidar dos animais?",
+    "Fluttershy é a pônei que adora cuidar dos animais"
+);
+
+criaCartao(
+    "My Little Pony",
+    "O que significa o *cutie mark* em *My Little Pony*?",
+    "O *cutie mark* representa o talento especial de cada pônei"
+);
